2ce5b8171af5b849ae27609bbda40f47a11b5a2c
kabirarshafidz <hafidzarsha@gmail.com>
2024-08-31 16:16:00 +0700

    upload to repo

--- index.js
+++ index.js
@@ -5,10 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import PopUpScreen from './src/screens/PopUpScreen';
-import SolutionScreen from './src/screens/SolutionScreen';
-import MainScreen from './src/screens/MainScreen';
-import ScreenPoint from './src/components/ScreenPoint';
-import Screen2 from './src/screens/Screen2';
 
 AppRegistry.registerComponent(appName, () => App);
